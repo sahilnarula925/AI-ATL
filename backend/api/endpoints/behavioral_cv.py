47dae863309877b2bb86148f7a07bc9dafe9ab7e
kr45732 <52721908+kr45732@users.noreply.github.com>
2024-10-26 16:19:19 -0400

    bavckend

--- backend/api/endpoints/behavioral_cv.py
+++ backend/api/endpoints/behavioral_cv.py
@@ -1,22 +1,29 @@
 import google.generativeai as genai
 import time
 from backend.api.utils.oauth import load_creds
-
+from backend.api.endpoints.behavorial_bq import evaluate_user_response
 
 creds = load_creds("cv")
 genai.configure(credentials=creds)
-model = genai.GenerativeModel('tunedModels/avbehavorialapi-rh48ho8em5cf')
+model = genai.GenerativeModel("tunedModels/avbehavorialapi-rh48ho8em5cf")
 
 
-def process_video(path: str, questions: str):
-    video = genai.upload_file(path=path)
+def process_video(path: str, question: str):
+    video_file = genai.upload_file(path)
 
     while video_file.state.name == "PROCESSING":
-        print('.', end='')
-        time.sleep(10)
+        time.sleep(2.5)
         video_file = genai.get_file(video_file.name)
 
+    prompt = "Positive Cues in AUDIO response to interview questions voice/speech patterns: 25/25 - Clear and Concise Communication25/25 - Professionalism in Speaking20/25 - Engaging & Enthusiastic25/25 - Confidence in KnowledgeNegative Cues in AUDIO response to interview questions (voice/speech patterns):Minus 5 Points - HesitationMinus 0 Points - NervousnessMinus 0 Points - Uncertainty"
 
-    response = model.generate_content("Positive Cues in AUDIO response to interview questions voice/speech patterns: 25/25 - Clear and Concise Communication25/25 - Professionalism in Speaking20/25 - Engaging & Enthusiastic25/25 - Confidence in KnowledgeNegative Cues in AUDIO response to interview questions (voice/speech patterns):Minus 5 Points - HesitationMinus 0 Points - NervousnessMinus 0 Points - Uncertainty")
-    print(response.text)
+    # response should contain evaluation of user video (scale 0 to 100) and speech to text
+    response = model.generate_content(
+        [video_file, prompt], request_options={"timeout": 600}
+    )
 
+    transcript = response["transcript"]  # do this
+
+    score, explanation = evaluate_user_response(transcript, question)
+
+    return score, explanation
--- backend/api/endpoints/behavorial_bq.py
+++ backend/api/endpoints/behavorial_bq.py
@@ -1,29 +1,45 @@
 from backend.api.utils.oauth import load_creds
-
 import google.generativeai as genai
 import os
 
 creds = load_creds("bq")
-
 genai.configure(credentials=creds)
+
 
 def generate_problem(question_number):
 
-    question_topics = ["", "tell me about yourself", "failure and improvement", "teamwork", "leadership", "problem solving", "adaptability and time management"]
+    question_topics = [
+        "tell me about yourself",
+        "failure and improvement",
+        "teamwork",
+        "leadership",
+        "problem solving",
+        "adaptability and time management",
+    ]
 
     model = genai.GenerativeModel("tunedModels/behavioral-interview--dbnow66ey5gn")
-    
-    prompt = "Give me 1 behavioral interview question with " + question_topics[question_number] + " as the main topic."
+
+    prompt = (
+        "Give me 1 behavioral interview question with "
+        + question_topics[question_number]
+        + " as the main topic."
+    )
     response = model.generate_content(prompt)
 
     return response.text
 
+
+# return score, explanation
 def evaluate_user_response(user_response, ideal_response):
-     
+
     model = genai.GenerativeModel("tunedModels/behavioral-interview--dbnow66ey5gn")
 
-    prompt = "Give me one number - a score out of 100 for how similar " + user_response + " is to " + ideal_response
+    prompt = (
+        "Give me one number - a score out of 100 for how similar "
+        + user_response
+        + " is to "
+        + ideal_response
+    )
     response = model.generate_content(prompt)
 
     return response
-    
