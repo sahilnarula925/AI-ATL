d2acb5a75d2b6d2832062fa5ad0a458b2ebaf2f7
Sahil Narula <91969203+sahilnarula925@users.noreply.github.com>
2024-10-26 15:10:36 -0400

    Update bq.py

--- backend/api/endpoints/bq.py
+++ backend/api/endpoints/bq.py
@@ -1,9 +1,9 @@
-from oauth import load_creds
+from backend.api.utils.oauth import load_creds
 
 import google.generativeai as genai
 import os
 
-creds = load_creds()
+creds = load_creds("bq")
 
 genai.configure(credentials=creds)
 
@@ -12,6 +12,8 @@ def generate_problem(question_number):
     question_topics = ["", "tell me about yourself", "failure and improvement", "teamwork", "leadership", "problem solving", "adaptability and time management"]
 
     model = genai.GenerativeModel("tunedModels/behavioral-interview--dbnow66ey5gn")
-    response = model.generate_content("Give me 1 behavioral interview question with ", question_topics[question_number], " as the main topic.")
+    
+    prompt = "Give me 1 behavioral interview question with " + question_topics[question_number] + " as the main topic."
+    response = model.generate_content(prompt)
 
     return response
